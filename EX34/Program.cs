07bd8523d14e24e14054820f5746403f68fd88c5
Olga <101519229+Olga2502@users.noreply.github.com>
2023-02-24 20:55:40 +0300

    Решение задачт 34

---
+++ EX34/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. Напишите 
+//программу, которая покажет количество чётных чисел в массиве.
+
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+
+
+int CountNumber(int[] array)
+{
+    int count = 0;
+    foreach(int element in array)
+    {
+        if (element % 2 == 0)
+        {
+            count++;
+        }
+    }
+return count;
+}
+
+int[] array = GetArray(8, 100, 1000);
+Console.WriteLine(String.Join(", ", array));
+
+int count = CountNumber(array);
+Console.WriteLine($"Количество четных чисел в массиве {count}");
