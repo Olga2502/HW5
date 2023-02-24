86a807d29d16857ac7542963ac87e8008df3e5a1
Olga <101519229+Olga2502@users.noreply.github.com>
2023-02-24 22:09:03 +0300

    Рещение задачи 36 более компактноо

--- EX36/Program.cs
+++ EX36/Program.cs
@@ -12,17 +12,6 @@ int[] GetArray(int size, int minValue, int maxValue)
     return result;
 }
 
-//int[] GetArray(int size, int minValue, int maxValue)
-//{
-//    int[] result = new int[size];
-//
-//    for (int i = 0; i < size; i++)
-//    {
-//        result[i] = new Random().Next(minValue, maxValue + 1);
-//    }
-//    return result;
-//}
-
 
 int Result(int[] inArray)
 {
